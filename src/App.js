0dddcd1a9c2a1fb40eae4d225870c5be8d8120b1
Jeffreywye <jeffreywye7@gmail.com>
2020-09-24 07:50:42 -0400

    added navbar and router code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import { BrowserRouter as Router, Link, Switch, Route, Redirect } from 'react-router-dom';
+import Upload from "./pages/Upload.js"
+import Home from "./pages/Home.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Gallery</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/upload">Upload</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/upload" component={Upload}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/UploadComponent/UploadComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UploadComponent = (props) => {
+    return (
+        <div>
+            <h1>Upload</h1>
+            <form></form>
+        </div>
+    );
+};
+
+export default UploadComponent;
